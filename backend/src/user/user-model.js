d8edc62c39e3394bd8e8800b15fee415e288404b
CIRIS Interface <cirisinterface@ncs.com.sg>
2024-02-04 00:31:29 +0800

    updatedeleteapi

--- backend/src/user/user-model.js
+++ backend/src/user/user-model.js
@@ -11,10 +11,32 @@ export async function getUser(id) {
     const [rows] = await pool.query(
         `SELECT * 
         FROM tbl_user
-        WHERE userId = ?`, [3]
+        WHERE userId = ?`, [id]
     );
     return rows[0];
 }
+
+export async function deleteUser(id) {
+    // First, select the data to be deleted
+    const [deletedUserData] = await pool.query(
+        `SELECT * FROM tbl_user
+        WHERE userId = ?`, [id]
+    );
+
+    // Now, delete the row
+    const [deletedRows] = await pool.query(
+        `DELETE FROM tbl_user
+        WHERE userId = ?`, [id]
+    );
+
+    if (deletedRows.affectedRows === 0) {
+        // If no rows were deleted, return null or throw an error
+        return null;
+    }
+    // Return the deleted data
+    return deletedUserData[0];
+}
+
 
 export async function createFunction(userId, age, dateJoined, firstName,lastName,email,education,username,password,birthDate,gender) {
     await pool.query(
