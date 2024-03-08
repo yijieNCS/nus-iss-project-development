fe19731e9031a57c1335163d0e7e963c66acdd68
Wong Yi Jie <d1359408@ncsdevnet.com>
2024-03-08 13:16:36 +0800

    Trial 1

--- backend/src/sessions/sessionController.controller.js
+++ backend/src/sessions/sessionController.controller.js
@@ -8,6 +8,7 @@ import {
 
 export async function getAllSessions(req, res) {
     try {
+        // Add Random Comment
         const sessions = await getSessionsModel()
         res.send(sessions)
     } catch (error) {
