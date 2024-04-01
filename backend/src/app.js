3275ca13d97fa80a6c455bf133dfca8e4fbf8999
Heng Yang Zhi NCS <yangzhi.heng@ncs.com.sg>
2024-04-01 09:55:54 +0800

    test

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 import sessionRouter from './sessions/sessionRoute.route.js'
 import resumeRouter from './resume/resumeRoute.route.js';
-import serviceRouter from './service/service-routes.js';u
+import serviceRouter from './service/service-routes.js';
 import userRouter from './user/userRoute.js';
 import registerRouter from "./register/register.route.js";
 import loginRouter from "./login/login.route.js";
