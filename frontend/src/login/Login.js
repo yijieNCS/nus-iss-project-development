9b2aa471e5d43fced4e044a6748d2682acb0e193
AtlasWongy <atlasyjyj@gmail.com>
2024-04-12 00:20:40 +0800

    Change PROD URL

--- frontend/src/login/Login.js
+++ frontend/src/login/Login.js
@@ -18,7 +18,7 @@ const Login = () => {
     const closeModal = () => setIsModalOpen(false)
 
     const navigate = useNavigate()
-    const serverUrl = process.env.REACT_APP_SERVER_URL_DEV
+    const serverUrl = process.env.REACT_APP_SERVER_URL_PROD
     console.log(`The server URL is ${serverUrl}`)
 
     const usernameRef = useRef()
