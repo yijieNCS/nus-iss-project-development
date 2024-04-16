1c455980219d3ee8e868b2b139a2bab80fb17302
Heng Yang Zhi NCS <yangzhi.heng@ncs.com.sg>
2024-04-16 15:20:49 +0800

    fixregui+Editsearchbarcss,js

--- frontend/src/services/searchbar/searchbar.js
+++ frontend/src/services/searchbar/searchbar.js
@@ -2,42 +2,41 @@ import React, { useState } from 'react';
 import styles from './searchbar.module.css'; 
 
 const SearchBar = ({ onSearch, onClear }) => {
-  const [query, setQuery] = useState('');
+    const [query, setQuery] = useState('');
 
-  const handleChange = (event) => {
-    setQuery(event.target.value);
-  };
+    const handleChange = (event) => {
+        setQuery(event.target.value);
+    };
 
-  const handleSubmit = (event) => {
-    event.preventDefault(); 
-    const searchBy = event.target.searchby.value; // This line is not needed
-    onSearch(query, searchBy); // Pass query and searchBy directly to onSearch
-  };
+    const handleSubmit = (event) => {
+        event.preventDefault(); 
+        const searchBy = event.target.searchby.value; // This line is not needed
+        onSearch(query, searchBy); // Pass query and searchBy directly to onSearch
+    };
 
-  const handleClear = () => {
-    setQuery(''); // Clear the query input
-    onClear(); // Call the onClear callback to reset search results
-  };
-  
-  return (
-    <form onSubmit={handleSubmit} className={styles["searchbar"]}>
-     <div>
-        <select name="searchby" id="searchby">
-            <option value="subject">Subject</option>
-            <option value="topic">Topic</option>
-        </select>
-        <input
-        type="text"
-        placeholder="Search..."
-        value={query}
-        onChange={handleChange}
-        />
-        <button type="submit">Search</button>
-        <button type="button" onClick={handleClear}>Clear</button>
-      </div>
-    </form>
-  );
+    const handleClear = () => {
+        setQuery(''); // Clear the query input
+        onClear(); // Call the onClear callback to reset search results
+    };
+   
+    return (
+        <form onSubmit={handleSubmit} className={styles["searchbar"]}>
+            <div className={styles["formContainer"]}> 
+                <select name="searchby" id="searchby">
+                    <option value="subject">Subject</option>
+                    <option value="topic">Topic</option>
+                </select>
+                <input
+                    type="text"
+                    placeholder="Search..."
+                    value={query}
+                    onChange={handleChange}
+                />
+                <button type="submit">Search</button>
+                <button type="button" onClick={handleClear}>Clear</button>
+            </div>
+        </form>
+    );
 };
 
 export default SearchBar;
-
